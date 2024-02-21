3b27a29e16cb78e1455c6fa837473854fe0425f4
unknown <andriesmathenjwa52@gmail.com>
2024-02-21 17:37:12 +0200

    Navigation bar updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import { FaBars } from 'react-icons/fa';
 const Navbar = () => {
     const [activeLink, setActiveLink] = useState('');
     const location = useLocation();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     React.useEffect(() => {
         setActiveLink(location.pathname);
@@ -14,11 +15,11 @@ const Navbar = () => {
 
     return (
         <nav className="navbar">
-            <div className="navbar-logo">
-                <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" className='navbar-logo' />
+            <div className="navbar-menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                <FaBars />
             </div>
-            <div className="navbar-links">
-                <ul>
+            <ul className={menuOpen ? "open" : "navbar-links"}>
                     <li><NavLink exact to="/" className={activeLink === "/" ? 'active-link' : ''}>Home</NavLink></li>
                     <li><NavLink to="/menu" className={activeLink === "/menu" ? 'active-link' : ''}>Menu</NavLink></li>
                     <li><NavLink to="/music" className={activeLink === "/music" ? 'active-link' : ''}>Music Section</NavLink></li>
@@ -26,11 +27,7 @@ const Navbar = () => {
                     <li><NavLink to="/contact" className={activeLink === "/contact" ? 'active-link' : ''}>Contact Us</NavLink></li>
                     <li><NavLink to="/gallery" className={activeLink === "/gallery" ? 'active-link' : ''}>Gallery</NavLink></li>
                     <li><NavLink to="/book" className={activeLink === "/book" ? 'active-link' : ''}>Book a Table</NavLink></li>
-                </ul>
-            </div>
-            <div className="navbar-menu-icon">
-                <FaBars />
-            </div>
+            </ul>
         </nav>
     );
 };
