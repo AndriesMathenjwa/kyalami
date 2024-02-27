53abaf8078cb0eac09abedd3c735ef6e079e0c91
unknown <andriesmathenjwa52@gmail.com>
2024-02-27 12:59:55 +0200

    updated gallery sub pages

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import About from './pages/about/About';
-import Home from './pages/home/Home';
-import Music from './pages/music/music';
-import Book from './pages/book/book';
-import Gallery from "./pages/gallery/Gallery"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//main pages imports
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Music from "./pages/music/music";
+import Book from "./pages/book/book";
+import Gallery from "./pages/gallery/Gallery";
+import Food from "./components/food/Food";
 
 //gigs imports
-import Friday from './pages/friday/friday';
-import Saturday from './pages/saturday/saturday';
-import Sunday from './pages/sunday/sunday';
+import Friday from "./pages/friday/friday";
+import Saturday from "./pages/saturday/saturday";
+import Sunday from "./pages/sunday/sunday";
 
 //Music imports
-import Soul from './pages/soul/soul';
-import Amapiano from './pages/amapiano/amapiano';
-import Deep from './pages/deep/deep';
-import Rnb from './pages/rnb/rnb';
-import Afropop from './pages/afropop/afropop';
-import Afrotech from './pages/afrotech/afrotech';
+import Soul from "./pages/soul/soul";
+import Amapiano from "./pages/amapiano/amapiano";
+import Deep from "./pages/deep/deep";
+import Rnb from "./pages/rnb/rnb";
+import Afropop from "./pages/afropop/afropop";
+import Afrotech from "./pages/afrotech/afrotech";
 
 function App() {
   return (
     <BrowserRouter>
-       <Routes>
-        <Route path="/" element={<Home/>}/> 
+      <Routes>
+        {/*pages*/}
+
+        <Route path="/" element={<Home />} />
         {/* <Route path="/menu" element={<Menu/>}/> */}
-          <Route path="/music" element={<Music/>}/>
-        <Route path="/about" element={<About/>}/>
+        <Route path="/music" element={<Music />} />
+        <Route path="/about" element={<About />} />
         {/* <Route path="/contact" element={<Contact/>}/> */}
-        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/gallery" element={<Gallery />} />
         {/* <Route path="/book" element={<Table/>}/> */}
-        <Route path="/book" element={<Book/>}/> 
+        <Route path="/book" element={<Book />} />
+        <Route path="/food" element={<Food />} />
 
-           {/*Gigs*/}
-           <Route path="/friday" element={<Friday/>}/> 
-        <Route path="/saturday" element={<Saturday/>}/> 
-        <Route path="/sunday" element={<Sunday/>}/> 
+        {/*Gigs*/}
+        <Route path="/friday" element={<Friday />} />
+        <Route path="/saturday" element={<Saturday />} />
+        <Route path="/sunday" element={<Sunday />} />
 
-         {/*Music*/}
-         <Route path="/soul" element={<Soul/>}/> 
-         <Route path="/amapiano" element={<Amapiano/>}/> 
-         <Route path="/deep" element={<Deep/>}/> 
-         <Route path="/rnb" element={<Rnb/>}/> 
-         <Route path="/afropop" element={<Afropop/>}/> 
-         <Route path="/afrotech" element={<Afrotech/>}/> 
-       </Routes>
+        {/*Music*/}
+        <Route path="/soul" element={<Soul />} />
+        <Route path="/amapiano" element={<Amapiano />} />
+        <Route path="/deep" element={<Deep />} />
+        <Route path="/rnb" element={<Rnb />} />
+        <Route path="/afropop" element={<Afropop />} />
+        <Route path="/afrotech" element={<Afrotech />} />
+      </Routes>
     </BrowserRouter>
   );
 }
