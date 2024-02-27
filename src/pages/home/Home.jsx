56feee88ae852a31aeaf666800e24189ae5a22ea
Musa <royaldesire10@gmail.com>
2024-02-27 23:24:30 +0200

    saving my initial project

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,18 @@ import Navbar from '../../components/navbar/Navbar'
 
 const Home = () => {
   return (
-    <div>
-        <Navbar/>
-    </div>
+    <>
+   <div className='navbar'>
+      <Navbar />
+   </div>
+
+   <div className='header'>
+      <h1>REDEFINING AFRICAN DAY VIBE</h1>
+   </div>
+  
+   </>
   )
 }
 
 export default Home
+
