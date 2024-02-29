fa0a4a55e1d935d06eda01c745c9cb863db37381
unknown <andriesmathenjwa52@gmail.com>
2024-02-29 08:56:55 +0200

    food subpage

--- src/pages/foods/FoodPage.jsx
+++ src/pages/foods/FoodPage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./foodPage.css";
 import Navbar from "../../components/navbar/Navbar";
+import Food from "../../components/food/Food";
+import { foodImages } from '../../files/food';
+import Footer from "../../components/footer/Footer";
 
 const FoodPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="food-heading">
-        <h1>Every bite, a taste of joy.</h1>
+      <div className="food-main">
+        <Food foodImages={foodImages}/>
+      </div>
+      <div className="fcontainer">
+        <Footer />
       </div>
     </div>
   );
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import "./home.css";
+import Footer from '../../components/footer/Footer';
+import { Carousel } from 'react-bootstrap';
+import LOGO1 from '../../photos/image00001.jpg';
+import LOGO2 from '../../photos/image00002.jpg';
+import LOGO3 from '../../photos/image00040.jpeg';
+import LOGO4 from '../../photos/image00010.jpg';
+import LOGO5 from '../../photos/kyalami-events.jpeg';
+import LOGO6 from '../../photos/outside.jpeg';
+import LOGO8 from '../../photos/image00003.jpg';
 
 const Home = () => {
+  
   return (
-    <div>
-        <Navbar/>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <div className='header'>
+        <h1>REDEFINING AFRICAN DAY VIBE</h1>
+      </div>
+      
+      <div className='Footer'>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
