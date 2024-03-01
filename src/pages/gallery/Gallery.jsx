d8e77bdb516b8ffa2b6d3f58cc377993e3f33d5c
unknown <andriesmathenjwa52@gmail.com>
2024-03-01 17:19:59 +0200

    menu page update

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 import "./gallery.css";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import foodImage from "../../photos/food.jpeg";
-import mix from "../../photos/mix.jpg";
-import music from "../../photos/music.jpg";
+// import foodImage from "../../photos/food.jpeg";
+// import mix from "../../photos/mix.jpg";
+// import music from "../../photos/music.jpg";
 
 const establishmentImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/establishment1.jpeg";
-// const foodImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/food.jpeg";
-// const mixImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/mix.jpg";
-// const musicImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/music.jpg";
+const foodImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/food.jpeg";
+const mixImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/mix.jpg";
+const musicImageUrl = "https://kyalami-menu.s3.eu-west-1.amazonaws.com/kyalami-pictures/Gallery/music.jpg";
 
 const Gallery = () => {
   return (
@@ -31,19 +31,19 @@ const Gallery = () => {
         <div className="image-container">
           <div className="image-wrapper">
             <NavLink to="/food">
-              <img src={foodImage} alt="Food" />
+              <img src={foodImageUrl} alt="Food" />
               <div className="image-text">Our food</div>
             </NavLink>
           </div>
           <div className="image-wrapper">
             <NavLink to="/food">
-              <img src={mix} alt="Mix" />
+              <img src={musicImageUrl} alt="Mix" />
               <div className="image-text">Mix of spirits</div>
             </NavLink>
           </div>
           <div className="image-wrapper">
             <NavLink to="/food">
-              <img src={music} alt="Music" />
+              <img src={mixImageUrl} alt="Music" />
               <div className="image-text">Live performances</div>
             </NavLink>
           </div>
